b644071880202610fc3c438fce3cb7ac987954c7
J-I-H-O <jeongjiho0731@gmail.com>
2022-12-25 22:03:57 +0900

    init

---
+++ 2022-12-23.R
@@ -0,0 +1,17 @@
+var1 <- 10
+var2 <- 20
+total <- var1 + var2
+
+print(total)
+
+num <-　2.2
+str <-　"Hello World!"
+bool <- TRUE
+nulltype <- NULL
+NAType <- NA
+NanType <- NaN
+
+numStr <- "2.2"
+print(numStr)
+numStr <- as.numeric(numStr)
+print(numStr)
